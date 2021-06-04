a1c6a41f5d6abb89ef71615aaefe4c42235d6cdd
Sebastien Le Digabel <sledigabel@gmail.com>
2021-06-04 16:57:06 +0100

    i know what i am doing

--- superscript.py
+++ superscript.py
@@ -7,3 +7,8 @@ This script does wonder
 if __name__ == '__main__':
     for _ in range(10):
         print("Not Funny")
+
+        
+        
+        
+        print("oh this one is gonna be a looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong one")
