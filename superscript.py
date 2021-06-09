7ef1eb4aee0f9726811fd0d5202daaadb28bd9b6
Sebastien Le Digabel <sledigabel@gmail.com>
2021-06-09 16:46:22 +0100

    forcing the test

--- superscript.py
+++ superscript.py
@@ -4,7 +4,16 @@
 This script does wonder
 """
 
+class hello:
+    def __init__(self, **kwargs, *args):
+        # print(**kwargs)
+        print(list(sorted(args,**kwargs)))
+
+
 if __name__ == '__main__':
     for _ in range(10):
         print("oh this one is gonna be a looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong one")
         print("Not Funny","no, really, not funny","I'm telling you, it's not", "really")
+        # hello("Not Funny","no, really, not funny","I'm telling you, it's not", "really")
+        hello(reverse=True, 1,4,6,3,7,2)
+
