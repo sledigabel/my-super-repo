4e8a5a82af3863db95474f1daaefc3627f2e03f2
Sebastien Le Digabel <sledigabel@gmail.com>
2021-06-17 11:20:30 +0100

    Fixing script

--- superscript.py
+++ superscript.py
@@ -4,16 +4,16 @@
 This script does wonder
 """
 
-class hello:
+
+class Hello:
+    """
+    Class Hello: a class that prints things
+    """
+
     def __init__(self, *args):
-        # print(**kwargs)
         print(list(sorted(args)))
 
 
 if __name__ == '__main__':
     for _ in range(10):
-        print("oh this one is gonna be a looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong one")
-        print("Not Funny","no, really, not funny","I'm telling you, it's not", "really")
-        # hello("Not Funny","no, really, not funny","I'm telling you, it's not", "really")
-        hello(1,4,6,3,7,2)
-
+        Hello(1, 4, 6, 3, 7, 2)
