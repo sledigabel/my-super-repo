5e52a190e5c5572a4059cab97892a670d380c817
Sebastien Le Digabel <sledigabel@gmail.com>
2021-06-17 09:36:26 +0100

    fixing the script

--- superscript.py
+++ superscript.py
@@ -5,9 +5,9 @@ This script does wonder
 """
 
 class hello:
-    def __init__(self, **kwargs, *args):
+    def __init__(self, *args):
         # print(**kwargs)
-        print(list(sorted(args,**kwargs)))
+        print(list(sorted(args)))
 
 
 if __name__ == '__main__':
@@ -15,5 +15,5 @@ if __name__ == '__main__':
         print("oh this one is gonna be a looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong one")
         print("Not Funny","no, really, not funny","I'm telling you, it's not", "really")
         # hello("Not Funny","no, really, not funny","I'm telling you, it's not", "really")
-        hello(reverse=True, 1,4,6,3,7,2)
+        hello(1,4,6,3,7,2)
 
